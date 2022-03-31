fc8510b47b3cfdd51d0dda93a1a0a1f3827136da
Tan Tran <tantan992@gmail.com>
2022-03-31 15:11:06 -0400

    Create project

---
+++ reminder/reminder/reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminderApp.swift
+//  reminder
+//
+//  Created by Tan Tan on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
