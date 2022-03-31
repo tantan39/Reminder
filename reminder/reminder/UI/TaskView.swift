8917547d0379ec3fe023ee4c5b5a9de799335038
Tan Tran <tantan992@gmail.com>
2022-03-31 15:27:41 -0400

    Create task list view

---
+++ reminder/reminder/UI/TaskView.swift
@@ -0,0 +1,76 @@
+//
+//  TaskView.swift
+//  reminder
+//
+//  Created by Tan Tan on 3/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum TaskPriority {
+    case low
+    case medium
+    case high
+}
+
+#if DEBUG
+let mockTasks = [
+  Task(title: "Learn about async/await", priority: .medium, completed: false),
+  Task(title: "Practice", priority: .medium, completed: false),
+  Task(title: "Practice more", priority: .high, completed: false),
+  Task(title: "Build side projects", priority: .high, completed: false)
+]
+#endif
+
+struct Task: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+    var priority: TaskPriority
+    var completed: Bool
+}
+
+struct TaskView: View {
+    var tasks: [Task] = mockTasks
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(tasks) { task in
+                        TaskCell(item: task)
+                    }
+                }
+                Text("Task list")
+                
+            }
+            .navigationTitle(Text("Reminder")
+                                .foregroundColor(.red))
+        }
+    }
+}
+
+struct TaskView_Preview: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
+
+struct TaskCell: View {
+    var item: Task
+    var body: some View {
+        HStack {
+            Image(systemName: "circle")
+                .foregroundColor(.gray)
+            Text(item.title)
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "info.circle")
+                    .foregroundColor(.blue)
+            }
+
+        }
+    }
+}
