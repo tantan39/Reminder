1b2d532a30b5097edefe699cacd8bde307799c10
Tan Tran <tantan992@gmail.com>
2022-03-31 18:53:05 -0400

    Move `Task` model to its own file

---
+++ reminder/reminder/Domain/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  reminder
+//
+//  Created by Tan Tan on 3/31/22.
+//
+
+import Foundation
+
+enum TaskPriority {
+    case low
+    case medium
+    case high
+}
+
+#if DEBUG
+let mockTasks = [
+  Task(title: "Learn about async/await", priority: .medium, completed: false),
+  Task(title: "Practice", priority: .medium, completed: false),
+  Task(title: "Practice more", priority: .high, completed: false),
+  Task(title: "Build side projects", priority: .high, completed: true)
+]
+#endif
+
+struct Task: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+    var priority: TaskPriority
+    var completed: Bool
+}
--- reminder/reminder/UI/TaskView.swift
+++ reminder/reminder/UI/TaskView.swift
@@ -8,28 +8,6 @@
 import Foundation
 import SwiftUI
 
-enum TaskPriority {
-    case low
-    case medium
-    case high
-}
-
-#if DEBUG
-let mockTasks = [
-  Task(title: "Learn about async/await", priority: .medium, completed: false),
-  Task(title: "Practice", priority: .medium, completed: false),
-  Task(title: "Practice more", priority: .high, completed: false),
-  Task(title: "Build side projects", priority: .high, completed: true)
-]
-#endif
-
-struct Task: Identifiable {
-    var id: String = UUID().uuidString
-    var title: String
-    var priority: TaskPriority
-    var completed: Bool
-}
-
 struct TaskView: View {
     @ObservedObject var viewModel = TasksViewModel()
     @State var presentAddNewItem = false
