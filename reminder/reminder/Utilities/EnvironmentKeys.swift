a6763813fae5c2920c3e979a48accc8dec8639c6
Tan Tran <tantt.dev@gmail.com>
2022-04-02 10:11:05 -0400

    Custom `EnvironmentKeys`

---
+++ reminder/reminder/Utilities/EnvironmentKeys.swift
@@ -0,0 +1,26 @@
+//
+//  EnvironmentKeys.swift
+//  reminder
+//
+//  Created by Tan Tan on 4/2/22.
+//
+
+import Foundation
+
+import Foundation
+import SwiftUI
+
+struct WindowKey: EnvironmentKey {
+  struct Value {
+    weak var value: UIWindow?
+  }
+  
+  static let defaultValue: Value = .init(value: nil)
+}
+
+extension EnvironmentValues {
+  var window: UIWindow? {
+    get { return self[WindowKey.self].value }
+    set { self[WindowKey.self] = .init(value: newValue) }
+  }
+}
