95df59bdb258bdc5020d58c2adbab5497ae7bf3f
Tan Tran <tantt.dev@gmail.com>
2022-04-04 12:54:48 -0400

    Move empty state view to its own file

---
+++ reminder/reminder/UI/EmptyStateView.swift
@@ -0,0 +1,29 @@
+//
+//  EmptyStateView.swift
+//  reminder
+//
+//  Created by Tan Tan on 4/4/22.
+//
+
+import SwiftUI
+
+struct EmptyStateViewModifier<EmptyContent>: ViewModifier where EmptyContent: View {
+    var isEmpty: Bool
+    let emptyContent: () -> EmptyContent
+    
+    func body(content: Content) -> some View {
+        if isEmpty {
+            emptyContent()
+        }
+        else {
+            content
+        }
+    }
+}
+
+extension View {
+    func emptyState<EmptyContent>(_ isEmpty: Bool,
+                                  emptyContent: @escaping () -> EmptyContent) -> some View where EmptyContent: View {
+        modifier(EmptyStateViewModifier(isEmpty: isEmpty, emptyContent: emptyContent))
+    }
+}
--- reminder/reminder/UI/TaskView.swift
+++ reminder/reminder/UI/TaskView.swift
@@ -81,24 +81,3 @@ struct TaskView_Preview: PreviewProvider {
         TaskView(viewModel: TasksViewModel())
     }
 }
-
-struct EmptyStateViewModifier<EmptyContent>: ViewModifier where EmptyContent: View {
-    var isEmpty: Bool
-    let emptyContent: () -> EmptyContent
-    
-    func body(content: Content) -> some View {
-        if isEmpty {
-            emptyContent()
-        }
-        else {
-            content
-        }
-    }
-}
-
-extension View {
-    func emptyState<EmptyContent>(_ isEmpty: Bool,
-                                  emptyContent: @escaping () -> EmptyContent) -> some View where EmptyContent: View {
-        modifier(EmptyStateViewModifier(isEmpty: isEmpty, emptyContent: emptyContent))
-    }
-}
